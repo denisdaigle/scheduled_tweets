2233020bd41ea9396d4372f5be3fd7b775abe271
Denis Daigle <denis@denisdaigle.com>
2021-02-08 19:26:27 -0400

    adding host for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,7 +117,9 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
-
+  
+  
+  config.action_mailer.default_url_options = { host: "https://scheduled-tweets-dd.herokuapp.com/" } 
   config.active_job.queue_adapter = :sidekiq
 
 end
